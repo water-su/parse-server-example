acbf0b94f30cee39ecaa22b6e7021d958ae50e05
water.su <pointou@gmail.com>
2016-04-27 12:02:27 +0800

    add maingun support

--- index.js
+++ index.js
@@ -22,7 +22,12 @@ var api = new ParseServer({
   restKey: process.env.REST_KEY,
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
+  },
+  emailAdapter: new SimpleMailgunAdapter({
+    apiKey: process.env.MAILGUN_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+    fromAddress: process.env.MAIL_FROM_ADDRESS
+  })
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
