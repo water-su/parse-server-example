f16f74badd3558e2d67a7f78f7bffc9bd3427133
water.su <pointou@gmail.com>
2016-04-27 13:14:56 +0800

    remove some mail setting

--- index.js
+++ index.js
@@ -32,12 +32,6 @@ var api = new ParseServer({
       apiKey: process.env.MAILGUN_API_KEY,
       domain: process.env.MAILGUN_DOMAIN,
       fromAddress: process.env.MAIL_FROM_ADDRESS,
-      customPages: {
-        invalidLink: 'http://localhost/invalid_link.html',
-        verifyEmailSuccess: 'http://localhost/verify_email_success.html',
-        choosePassword: 'http://localhost/choose_password.html',
-        passwordResetSuccess: 'http://localhost/password_reset_success.html'
-      }
     }
   }
 });
