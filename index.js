05998f931b12415c6fd666f2e2158d90ea806cc3
water.su <pointou@gmail.com>
2016-05-10 19:33:17 +0800

    try fix s3

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var S3Adapter = require('parse-server').S3Adapter;
-var s3Adapter = new S3Adapter( process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY, process.env.S3_BUCKET);
+var s3Adapter = new S3Adapter( process.env.S3_ACCESS_KEY, process.env.S3_SECRET_KEY, process.env.S3_BUCKET, {directAccess:true});
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
